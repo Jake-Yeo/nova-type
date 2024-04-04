7bcc92955f0c72f643ad5fd0e04e2e6f5de17b29
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-04-04 12:00:39 -0700

    Basic typing functionality checking

--- src/components/TextDisplay.tsx
+++ src/components/TextDisplay.tsx
@@ -1,7 +1,16 @@
+import Typography from '@mui/material/Typography';
 import * as React from 'react'
 
 export default function TypeArea({ textToType }: { textToType: string}) {
+    
+    const rows = [];
+
+    for (let i = 0; i < 1; i++) {
+        rows.push(<span style ={{color: 'red'}}>Hello</span>);
+    }
+    
     return (<>
     <div>{textToType}</div>
+    <div>{rows}</div>
     </>);
 }
