b0c2c224b2ac2913a44cc75f4b4286ae0a698723
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-05-07 16:00:49 -0700

    Fixed very buggy scrolling for the history page <VariableSizeList> component by passing in the style prop props.style  https://stackoverflow.com/questions/56737563/react-window-and-infinite-loader-scrolling-issue

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -1,7 +1,7 @@
 //@ts-ignore
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import { FixedSizeList, ListChildComponentProps, VariableSizeList } from 'react-window';
 import { currentUser } from '../objects/User';
-import { Box, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { Box, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 import { TypingDataContext } from './TypeFeedAreaDisplay';
 import { useContext, useReducer } from 'react';
 import React from 'react';
@@ -11,15 +11,20 @@ const HistoryList = () => {
 
     const [ignored, forceUpdate] = useReducer(x => x + 1, 0); // https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
 
+    // !!! When using React-Window components don't forget to pass in the props.style!! https://stackoverflow.com/questions/56737563/react-window-and-infinite-loader-scrolling-issue
     const ListItemComponent = (props: ListChildComponentProps) => { // So how this works I think is that the FixSizeList will pass props into this automatically. The props contains an index, we can use this index to get the right stat.
 
-
-        return (<ListItem key={1} component="div" disablePadding>
-            <ListItemButton>
-                <ListItemText primary={`Item ${currentUser.getTypingStats().at(props.index)?.getGeneratedPrompt()}`} />
-            </ListItemButton>
-        </ListItem>)
+        return (
+            <div style={props.style}>
+                <ListItem key={props.index} component="div" disablePadding>
+                     <ListItemText primary={`Item ${currentUser.getTypingStats().at(props.index)?.getGeneratedPrompt()}`} />
+                </ListItem>
+            </div>)
     };
+
+    const getSizeOfComponentAtIndex = (index: number) => {
+        return 300;
+    }
 
     React.useEffect(() => { //https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
         forceUpdate();
@@ -29,15 +34,15 @@ const HistoryList = () => {
         <Box
             sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
         >
-            <FixedSizeList
+            <VariableSizeList 
                 height={400}
                 width={360}
-                itemSize={46}
+                itemSize={getSizeOfComponentAtIndex}
                 itemCount={currentUser.getTypingStats().length}
                 overscanCount={5}
             >
                 {ListItemComponent}
-            </FixedSizeList>
+            </VariableSizeList >
         </Box>
     </>)
 }
