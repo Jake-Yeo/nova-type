a764ae26664c02cf7687b5cc4e53e04b18fde461
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-05-08 16:24:28 -0700

    Fixed buggy scrolling dynamic height component bug. Where we were getting undefined heights when trying to set the dynamic heights of the VariableSizeList components. Mainly used this resource to help fix the bug. https://codesandbox.io/p/sandbox/react-window-dynamic-row-height-8ftbq?file=%2Fsrc%2FApp.js%3A37%2C10

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -3,128 +3,85 @@ import { FixedSizeList, ListChildComponentProps, VariableSizeList } from 'react-
 import { currentUser } from '../objects/User';
 import { Box, Grid, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 import { TypingDataContext } from './TypeFeedAreaDisplay';
-import { useContext, useEffect, useReducer, useRef, useState } from 'react';
+import { useCallback, useContext, useEffect, useReducer, useRef, useState } from 'react';
 import React from 'react';
 import DynamicColorNumberDisplay from './DynamicColorNumberDisplay';
 import { getToTypeDisplayPublic } from './ToTypeDisplay';
+import HistoryStatComponent from './HistoryStatComponent';
+
+
+// Sandbox below to help you understand how to deal with dynamic component heights
+//https://codesandbox.io/p/sandbox/react-window-dynamic-row-height-8ftbq?file=%2Fsrc%2FApp.js%3A37%2C10
+//https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
+// !!! When using React-Window components don't forget to pass in the props.style!! https://stackoverflow.com/questions/56737563/react-window-and-infinite-loader-scrolling-issue
+
+
+//https://codesandbox.io/p/sandbox/react-window-dynamic-row-height-8ftbq?file=%2Fsrc%2FApp.js%3A37%2C10
+// THIS Code is largely modeled off of the sandbox above. Tbh I'm not too sure how this code works
+    // I beleive that when the index or add size function is changed, it calls the addSize function which is able to access the listRef. Then we are able to add the heights to the arry
+    // In the addSize function, it also calls listRef.current?.resetAfterIndex(index); which is effectively just a force re-render of this component
+
+const width = 85;
+
+//const [ignored, forceUpdate] = useReducer(x => x + 1, 0); // https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
+
+var listItemComponentSize: number[] = [];
+
+interface ListItemComponentPropsExtended extends ListChildComponentProps {
+    addSize: (height: number, index: number) => void;
+}
+
+// !!! When using React-Window components don't forget to pass in the props.style!! https://stackoverflow.com/questions/56737563/react-window-and-infinite-loader-scrolling-issue
+const ListItemComponent = ({ index, style, data, addSize }: ListItemComponentPropsExtended) => { // So how this works I think is that the FixSizeList will pass props into this automatically. The props contains an index, we can use this index to get the right stat.
+    const licRef = useRef<HTMLDivElement>(null);
+
+
+    const listItem = (
+        // So because the div takes on the style of the prop, we don't know what its height is styled to, so we useRef on the ListItem instead to get the height
+        <div style={style}>
+            <ListItem ref={licRef} onClick={(e) => (console.log(licRef.current?.clientHeight))} key={index} component="div" disablePadding>
+                <ListItemText primary={<HistoryStatComponent index={index} width={width} />} />
+            </ListItem>
+        </div>)
+
+    React.useEffect(() => {
+        addSize(licRef.current?.clientHeight ?? 0, index);
+    }, [addSize, index]);
+
+    return (listItem);
+};
 
 
 const HistoryList = () => {
 
+    const listRef = useRef<VariableSizeList>(null);
 
-    const typingData = useContext(TypingDataContext);
-
-
-    const [ignored, forceUpdate] = useReducer(x => x + 1, 0); // https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
-
-
-    const width = 85;
-
-    const statComponentRefs: React.RefObject<HTMLDivElement>[] = [];
-
-    const [statComponentHeights, setStatComponentHeights] = useState<number[]>([]);
-
-
-    const getStatComponent = (statIndex: number) => { // make sure to log the height of the component
-        const typingStat = currentUser.getTypingStats().at(statIndex);
-
-
-        let wpmComponent: JSX.Element | null = null;
-        let accuracyComponent: JSX.Element | null = null;
-        let accuracyVisualization: JSX.Element | null = null;
-
-
-        if (typingStat) {
-            wpmComponent = <DynamicColorNumberDisplay stat={typingStat.getWpm()} statName={'WPM:'}></DynamicColorNumberDisplay>
-            accuracyComponent = <DynamicColorNumberDisplay stat={typingStat.getAccuracy()} statName={'Accuracy:'}></DynamicColorNumberDisplay>
-            accuracyVisualization = <>{getToTypeDisplayPublic(typingStat.getGeneratedPrompt(), typingStat.getTypedPrompt())}</>
-        }
-
-
-        const scrollableAccuracyVisualization = <div className="scrollCss"
-            key='scrollPane'
-            contentEditable={false}
-            style={{
-                display: 'inline-block',
-                width: `${width}vw`, // + converts a data type object Number to primitive type number
-                overflow: 'hidden',
-                whiteSpace: 'normal', // Enable text wrap
-                wordWrap: 'break-word', // Allow breaking long words
-                overflowY: 'scroll' // Use camelCase for hyphenated CSS properties
-            }}>
-            {accuracyVisualization}
-        </div>
-
-
-        return (<>
-            <Grid container
-                alignItems={'flex-start'}
-                direction={'column'}
-            >
-                {wpmComponent}
-                {accuracyComponent}
-                {scrollableAccuracyVisualization}
-            </Grid>
-        </>)
+    const addSize = (height: number, index: number) => { // This will be called by the ListItemComponent function when index or addSize change state. They change state after they mount I beleive which is why this works. In previous versions all our attemps called or tried to re-render befor the components mounted! That is why we were getting undefined heights!
+        listItemComponentSize.push(height); // We add height to the listItemComponentSize array
+        listRef.current?.resetAfterIndex(index); // Effectivly just a re-render
     }
 
-
-    // !!! When using React-Window components don't forget to pass in the props.style!! https://stackoverflow.com/questions/56737563/react-window-and-infinite-loader-scrolling-issue
-    const ListItemComponent = (props: ListChildComponentProps) => { // So how this works I think is that the FixSizeList will pass props into this automatically. The props contains an index, we can use this index to get the right stat.
-
-        const licRef = useRef<HTMLDivElement>(null);
-
-
-        const listItem = (
-            // So because the div takes on the style of the prop, we don't know what its height is styled to, so we useRef on the ListItem instead to get the height
-            <div style={props.style}> 
-                <ListItem ref={licRef} onClick={(e) => (console.log(licRef.current?.clientHeight))} key={props.index} component="div" disablePadding>
-                    <ListItemText primary={getStatComponent(props.index)} />
-                </ListItem>
-            </div>)
-
-        statComponentRefs.push(licRef);
-
-        return (listItem);
+    const getSize = (index: number) => {
+        return listItemComponentSize.at(index) || 0; // This is the getSize function
     };
-
-
-    const getSizeOfComponentAtIndex = (index: number) => {
-        console.log('Returned height', statComponentHeights.at(index));
-        return statComponentHeights.at(index) ?? 0;
-    }
-
-
-    React.useEffect(() => { //https://stackoverflow.com/questions/46240647/how-to-force-a-functional-react-component-to-render
-        forceUpdate();
-        console.log('forced update');
-        console.log('useeffect get height',statComponentRefs.at(0)?.current?.clientHeight);
-
-        const newStatComponentHeights = [];
-
-        for (const ref of statComponentRefs) {
-            console.log('height in for loop', ref.current?.clientHeight ?? 0);
-            newStatComponentHeights.push(ref.current?.clientHeight ?? 0); // use coalesing operator, if the left side of ?? is undefined, then return a height of 0
-        }
-
-        setStatComponentHeights(newStatComponentHeights);
-
-    }, [typingData.toType]); // Update everytime toType changes (Usually toType changes when the user finishes typing the prompt, so this is a good way to update the history)
 
     return (<>
         <Box
             sx={{ width: `${width}vw`, height: 400, bgcolor: 'background.paper' }}
         >
             <VariableSizeList
+                ref={listRef}
                 height={400}
                 width={`${width}vw`}
-                itemSize={(index: number) => getSizeOfComponentAtIndex(index)}
+                itemSize={(index: number) => getSize(index)}
                 itemCount={currentUser.getTypingStats().length}
                 overscanCount={5}
             >
-                {ListItemComponent}
+                {({ index, style }) => (
+                    <ListItemComponent index={index} style={style} data={undefined} addSize={addSize}></ListItemComponent>
+                )}
             </VariableSizeList >
-        </Box>
+        </Box >
     </>)
 }
 
---
+++ src/components/HistoryStatComponent.tsx
@@ -0,0 +1,54 @@
+import { Grid } from "@mui/material";
+import { currentUser } from "../objects/User";
+import DynamicColorNumberDisplay from "./DynamicColorNumberDisplay";
+import { getToTypeDisplayPublic } from "./ToTypeDisplay";
+
+interface props {
+    index: number,
+    width: number
+}
+
+const HistoryStatComponent = ({index, width}: props) => {// make sure to log the height of the component
+        const typingStat = currentUser.getTypingStats().at(index);
+
+
+        let wpmComponent: JSX.Element | null = null;
+        let accuracyComponent: JSX.Element | null = null;
+        let accuracyVisualization: JSX.Element | null = null;
+
+
+        if (typingStat) {
+            wpmComponent = <DynamicColorNumberDisplay stat={typingStat.getWpm()} statName={'WPM:'}></DynamicColorNumberDisplay>
+            accuracyComponent = <DynamicColorNumberDisplay stat={typingStat.getAccuracy()} statName={'Accuracy:'}></DynamicColorNumberDisplay>
+            accuracyVisualization = <>{getToTypeDisplayPublic(typingStat.getGeneratedPrompt(), typingStat.getTypedPrompt())}</>
+        }
+
+
+        const scrollableAccuracyVisualization = <div className="scrollCss"
+            key='scrollPane'
+            contentEditable={false}
+            style={{
+                display: 'inline-block',
+                width: `${width}vw`, // + converts a data type object Number to primitive type number
+                overflow: 'hidden',
+                whiteSpace: 'normal', // Enable text wrap
+                wordWrap: 'break-word', // Allow breaking long words
+                overflowY: 'scroll' // Use camelCase for hyphenated CSS properties
+            }}>
+            {accuracyVisualization}
+        </div>
+
+
+        return (<>
+            <Grid container
+                alignItems={'flex-start'}
+                direction={'column'}
+            >
+                {wpmComponent}
+                {accuracyComponent}
+                {scrollableAccuracyVisualization}
+            </Grid>
+        </>)
+} 
+
+export default HistoryStatComponent;
