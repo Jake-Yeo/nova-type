21dcb324a167196966813e4257868937797d9d72
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-05-13 14:25:42 -0700

    Added styling for the loading page.

--- src/Pages/LoadingPage.tsx
+++ src/Pages/LoadingPage.tsx
@@ -1,11 +1,28 @@
-import { Typography } from "@mui/material"
+import { Box, Stack, Typography } from "@mui/material"
 
 const LoadingPage = () => {
 
-
+    const width = 65;
+    const height = width * 1 / 2;
 
     return (<>
-    <Typography color='white'>loading...</Typography>
+        <Stack justifyContent={'center'}
+            alignItems={'center'}
+            sx={{
+                width: '100vw',
+                height: '100vh'
+            }}>
+            <Typography color='white' fontSize={width}>Loading...</Typography>
+            <Box
+                sx={{
+                    backgroundImage: 'url("./svgFiles/infinite-spinner.svg")', // Load background image
+                    backgroundSize: 'contain', // Scale the background image to fit within the container while preserving its aspect ratio
+                    backgroundRepeat: 'no-repeat',
+                    width: `${width}vw`, // Set the width of the container
+                    height: `${height}vw` // Automatically adjust the height based on the aspect ratio
+                }}
+            />
+        </Stack>
     </>)
 }
 
