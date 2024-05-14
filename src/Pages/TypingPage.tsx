ac93b577f593ee8dd5479fb17dfc13cb775ad6a5
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-05-14 11:42:20 -0700

    Fixed wave animation positioning when screen size is small horizontally. There is a zooming in glitch though. If you zoom in, the wave animations no longer stack on top of each other perfectly for some reason.

--- src/Pages/TypingPage.tsx
+++ src/Pages/TypingPage.tsx
@@ -1,4 +1,4 @@
-import { Grid, Stack } from "@mui/material"
+import { Box, Grid, Stack } from "@mui/material"
 import LogoNavBar from "../components/LogoNavBar"
 import SettingsIsland from "../components/SettingsIsland"
 import TypeFeedAreaDisplay from "../components/TypeFeedAreaDisplay"
@@ -9,23 +9,16 @@ import TwoPeakWaveSvg from "../components/TwoPeakWaveSvg"
 
 const TypingPage = () => {
     return (<>
-        <Stack
+        <Stack // Please fix overflow on mobile, they cant scroll!
             direction='column'
             justifyContent='space-between'
             height='100vh'
             width='100vw'
             sx={{
-                overflow: 'hidden',
+                overflowX: 'hidden',
                 position: 'relative'
             }}
         >
-            {getWaveAnimation('7vh', 0.25, 'forwards', 8, OnePeakWaveSvg)}
-            {getWaveAnimation('10vh', 0.25, 'forwards', 5, TwoPeakWaveSvg)}
-            {getWaveAnimation('13vh', 0.25, 'backwards', 9, TwoPeakWaveSvg)}
-
-            {getWaveAnimation('15vh', 0.25, 'backwards', 11, OnePeakWaveSvg)}
-            {getWaveAnimation('10vh', 0.25, 'forwards', 8, TwoPeakWaveSvg)}
-            {getWaveAnimation('16vh', 0.25, 'forwards', 15, TwoPeakWaveSvg)}
             <Grid
                 container
                 direction='column'
@@ -38,6 +31,23 @@ const TypingPage = () => {
             </Grid>
             <Grid item>
                 <LinksDisplay></LinksDisplay>
+                <Box
+                    sx={{
+                        position: 'relative',
+                        bottom: 0,
+                        width: '100vw',
+                        margin: 0, // Set margin to 0 to remove any default spacing
+                        padding: 0, // Set padding to 0 to remove any default padding
+                    }}
+                >
+                    {getWaveAnimation('7vh', 0.25, 'forwards', 8, OnePeakWaveSvg)}
+                    {getWaveAnimation('10vh', 0.25, 'forwards', 5, TwoPeakWaveSvg)}
+                    {getWaveAnimation('13vh', 0.25, 'backwards', 9, TwoPeakWaveSvg)}
+
+                    {getWaveAnimation('15vh', 0.25, 'backwards', 11, OnePeakWaveSvg)}
+                    {getWaveAnimation('10vh', 0.25, 'forwards', 8, TwoPeakWaveSvg)}
+                    {getWaveAnimation('16vh', 0.25, 'forwards', 15, TwoPeakWaveSvg)}
+                </Box>
             </Grid>
         </Stack>
     </>)
